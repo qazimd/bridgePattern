98d24f8d93ff6a4cff0a1ed7a218a94a27fd11ce
Qazim Dervishi <qazim.dervishi003@gmail.com>
2023-11-26 13:08:31 +0100

    Message is the abstraction, which contains a reference to the implementor

---
+++ Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class Message
+{
+    protected IMessageSender messageSender;
+
+    protected Message(IMessageSender sender)
+    {
+        messageSender = sender;
+    }
+
+    public abstract void Send();
+}
