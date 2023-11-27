d4e56e764505bf1118f176371515ffa6972a7c92
Qazim Dervishi <qazim.dervishi003@gmail.com>
2023-11-27 20:02:49 +0100

    EmailSender and SmsSender are concrete implementors that provide specific mechanisms for sending messages.

---
+++ EmailSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class EmailSender : IMessageSender
+{
+    public void SendMessage(string subject, string body)
+    {
+        // Logic to send email
+        Console.WriteLine($"Sending Email: Subject: {subject}, Body: {body}");
+    }
+}
---
+++ SmsSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SmsSender : IMessageSender
+{
+    public void SendMessage(string subject, string body)
+    {
+        // Logic to send SMS
+        Console.WriteLine($"Sending SMS: Message: {body}");
+    }
+}
