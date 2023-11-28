cb64077dbfedd88614825317f40238974ef2f0be
Qazim Dervishi <qazim.dervishi003@gmail.com>
2023-11-28 12:07:14 +0100

    Rest of files

---
+++ SystemMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SystemMessage : Message
+{
+    private string messageContent;
+
+    public SystemMessage(string content, IMessageSender sender) : base(sender)
+    {
+        messageContent = content;
+    }
+
+    public override void Send()
+    {
+        string fullMessage = $"[System] {messageContent}";
+        messageSender.SendMessage("System Alert", fullMessage);
+    }
+}
---
+++ UserMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class UserMessage : Message
+{
+    private string subject;
+    private string body;
+
+    public UserMessage(string subject, string body, IMessageSender sender) : base(sender)
+    {
+        this.subject = subject;
+        this.body = body;
+    }
+
+    public override void Send()
+    {
+        messageSender.SendMessage(subject, body);
+    }
+}
